46d38d4c4dc98c1352a9b944c96f863d8fa80a9e
Pikhardo <rg2723949@gmail.com>
2025-08-28 09:56:34 -0300

    codigo revisado e com comentarios

--- criarBD.sql
+++ criarBD.sql
@@ -1,10 +1,10 @@
-
+-- cria db
 create DATABASE fatec;
-
+-- seleciona ela para uso
 use fatec;
 
 
-
+-- cria a tabela produtos 
 create table produtos(
     codigo smallint UNSIGNED not NULL, 
     nome varchar(100) not NULL, 
@@ -14,5 +14,5 @@ create table produtos(
     primary key (codigo), 
     unique KEY(nome)
     );
-
+-- comando para descrever a tabela
 desc produtos;
